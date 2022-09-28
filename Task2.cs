5fef8db95b82368aba0ad8438b4226ea3c6308d1
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-28 12:22:04 +0300

    Update Task2.cs

--- Task2.cs
+++ Task2.cs
@@ -12,38 +12,29 @@ Console.Write("Введите второе значение: ");
 bool boolM = int.TryParse(Console.ReadLine(), out int numberM);
 
 //Check 
-if (!boolN)
-    Console.WriteLine("Не верно введены значения! Введите число!");
-else
+if (boolN && boolM)
 {
     n = numberN;
-}
-if (!boolM)
-    Console.WriteLine("Не верно введены значения! Введите число!");
-else
-{
     m = numberM;
-}
-int temp = 0;
-if (n > m)
-{
-    temp = n;
-    n = m;
-    m = temp;
-}
-
-//sum of range numbers from recursion
-int sumOfRangeNumbers(int n, int m)
-{
-    int sum = m;
+    int temp = 0;
+    if (n > m)
     {
+        temp = n;
+        n = m;
+        m = temp;
+    }
+    //sum of range numbers from recursion
+    int sumOfRangeNumbers(int n, int m)
+    {
+        int sum = m;
         if (m > n)
         {
             sum += sumOfRangeNumbers(n, m - 1);
         }
         return sum;
     }
+    int result = sumOfRangeNumbers(n, m);
+    Console.WriteLine(result);
 }
-
-int result = sumOfRangeNumbers(n, m);
-Console.WriteLine(result);
+else
+    Console.WriteLine("Не верно введены значения! Введите число!");
